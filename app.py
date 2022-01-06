c8ed692f41ecb86e1488364c86ef1d7ca0d2796a
samadka <91091665+samadka@users.noreply.github.com>
2022-01-06 15:56:48 +0400

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ df['day'] = df["date"].dt.day
 
 
 
-selected_year = st.sidebar.selectbox('Year', list(reversed(range(2010,2022))))
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(2010,2023))))
 test = {"January":1, "February":2, "March":3, "April":4, "May":5, "June":6, "July":7, "August":8, "September":9, "October":10,
 "November":11, "December":12}
 selected_month = st.sidebar.selectbox('Months:', test.keys())
